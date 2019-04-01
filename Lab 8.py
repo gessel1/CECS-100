dffb66f6e09af77f4dd55d6648bacee824f45371
Gideon Essel <47682214+gessel1@users.noreply.github.com>
2019-03-31 21:42:03 -0700

    Add files via upload

---
+++ Lab 8.py
@@ -0,0 +1,34 @@
+#Female
+print( "How much do you weigh in pounds?")
+
+weight= int(input('weight:'))
+
+print('What is your height in inches?')
+      
+height= int(input('height:'))
+
+print('How many years in age are you?')
+
+age= int(input('age:'))
+
+BMR =(655 + ( 4.3 * weight)+ (4.7 * height)-(4.7 * age))
+
+print(BMR)
+    
+
+#Male
+print( "How much do you weigh in pounds?")
+
+weight= int(input('weight:'))
+
+print('What is your height in inches?')
+      
+height= int(input('height:'))
+
+print('How many years in age are you?')
+
+age= int(input('age:'))
+
+BMR =(66 + ( 6.3 * weight)+ (12.9 * height)-(6.8 * age))
+
+print(BMR)
