a230b2c4f414b8531694f362a07bdebc436c75cd
Gideon Essel <47682214+gessel1@users.noreply.github.com>
2019-03-31 22:06:10 -0700

    Add files via upload

---
+++ CECS HW 6.py
@@ -0,0 +1,23 @@
+
+def getIntVal():
+    print('Pick a number from 1-15')
+    val = int(input('Enter your choice: '))
+    if val == 7:
+        for t in range(7):
+            for x in range(t): 
+                print(' ',end='')
+            print('##')
+
+def val():
+    val = int(input('Enter your choice: '))
+
+def pattern2(val):
+    getIntVal()
+    
+
+    
+pattern2(val)
+
+    
+    
+
