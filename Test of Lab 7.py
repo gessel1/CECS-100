fd6a8e8284501b28e474baaffe35a0a70890d93d
Gideon Essel <47682214+gessel1@users.noreply.github.com>
2019-03-31 21:40:44 -0700

    Add files via upload

---
+++ Test of Lab 7.py
@@ -0,0 +1,24 @@
+#Gideon Essel
+#Lab 7
+
+#We need to start with prompting of the message screen starting with the print command
+print('Good morning. What is your name?')
+
+#It asks for our name next so we can define name using an input command
+name = input()
+
+#We now need to output a personalized greeting message
+#We can do that with a print statement keeping in mind that a comma seperates things
+
+#Example statement: print(' Have a nice day', name(our defined function from before, 'How are you today?')
+
+#print('Have a nice day', name, 'How are you today?')
+
+
+#We see that it works so lets try it out in the statement
+
+print('Hello',name, ',','How are you today?')
+
+
+
+
